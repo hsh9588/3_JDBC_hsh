691d97f3902c1c07e2c3af1eb4022b3777ce1288
hsh9588 <hsh9588@naver.com>
2022-09-05 17:40:52 +0900

    20220905 JDBC 수업

---
+++ jdbc1/src/edu/kh/jdbc1/JDBCExample3.java
@@ -0,0 +1,32 @@
+package edu.kh.jdbc1;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class JDBCExample3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+//		try {
+//			
+//			Class.forName("oracle.jdbc.driver.OracleDriver");
+//			
+//		} catch (ClassNotFoundException e) {
+//			
+//		} catch() {
+//			
+//		} finally {
+//			
+//		}
+		
+	}
+
+}
